dddc7b2b11ab3529442e95a933de42c39fc0b563
olusoga olalekan <olusogaolamileksy@gmail.com>
2024-08-02 00:19:15 +0100

    refactor

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CustomLogger } from './customLogger/custom_logger.service';
 import helmet from 'helmet';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  
   app.use(helmet());
-  app.enableCors({
-    origin: '*',
-    methods: 'GET',
-    credentials: true,
-  });
-
+  app.useGlobalFilters(new HttpExceptionFilter());
   const customLogger = app.get(CustomLogger);
   app.useLogger(customLogger)
+
   await app.listen(3000);
 }
 bootstrap();
