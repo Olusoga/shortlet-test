39a46c9b93eafe87cd14e1ae097993cfcc981d58
olusoga olalekan <olusogaolamileksy@gmail.com>
2024-08-02 17:54:42 +0100

    made changes on redis service

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -6,9 +6,18 @@ export class RedisService implements OnModuleInit, OnModuleDestroy {
   private client: Redis
 
   onModuleInit() {
+    const redisHost = process.env.REDIS_HOST || '127.0.0.1';
+    const redisPort = parseInt(process.env.REDIS_PORT, 10) || 6379;
+    const redisPassword = process.env.REDIS_PASSWORD || ''
+
     this.client = new Redis({
-      host: 'localhost',   
-      port: 6379,          
+      host: redisHost,
+      port: redisPort,
+      password: redisPassword,
+    });
+
+    this.client.on('error', (error) => {
+      console.error('[ioredis] Unhandled error event:', error);
     });
   }
 
